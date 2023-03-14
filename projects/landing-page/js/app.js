7351423acd7a1a7d6eec0320db69cdeae2e4de46
Exodion <113134972+Exodion@users.noreply.github.com>
2023-03-14 13:54:48 +0100

    Project ready

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -22,41 +22,91 @@
  * Define Global Variables
  * 
 */
+let allSections;
+let section;
+let menuItems = [];
+let x = 1; // Section number
+let y = 1; // Section number
+let link = "";
+const fragment = document.createDocumentFragment();
+let coordinates = [];
+let coordtop, coordleft;
 
+let allAnchors;
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
 // build the nav
+allSections = document.querySelectorAll("section");
 
+for (let section of allSections){
+    let heading = "";
+    let ident = `section${x}`;
+
+
+    heading = section.querySelector("h2").textContent;
+    link = `<a href="#${ident}">${heading}</a>`;
+
+    //create li tag
+    newLi = document.createElement("li");
+    newLi.setAttribute("id",`menu-link-${x}`);
+    newLi.addEventListener("click",console.log("test"));
+    newLi.innerHTML = link;
+    menuItems[x-1] = newLi;
+
+    console.log(link);
+
+
+
+    x = x + 1;
+}
+
+for (let menuItem of menuItems){
+
+        fragment.appendChild(menuItem);
+
+    }
+
+document.querySelector("#navbar__list").appendChild(fragment); 
+
+
+
+allAnchors = document.querySelectorAll('a[href^="#"]');
+
+for (let anchorItem of allAnchors){
+
+    anchorItem.addEventListener("click", function(event){
+        event.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+            behavior: "smooth"
+        }
+
+
+        )
+
+        
+
+    })
+
+}
 
 // Add class 'active' to section when near top of viewport
+document.addEventListener("scroll", function(event){
+        for (const section of allSections) {
+        const box = section.getBoundingClientRect();
 
+        if (box.top <= 200 && box.bottom >= 150) {
+        //apply active state on current section and corresponding Nav link
+        section.setAttribute("class","active");
+        section.classList.add("highlight");
 
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
+        } else {
+        //Remove active state from other section and corresponding Nav link
+        section.setAttribute("class","");
+        
+        }
+        }
+    })
 
 
